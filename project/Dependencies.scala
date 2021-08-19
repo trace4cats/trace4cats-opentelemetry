222eb040899ae73275e6f3415dd92335778f2bd5
Scala Steward <me@scala-steward.org>
2021-08-19 06:30:28 +0200

    Reformat with scalafmt 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,18 +23,18 @@ object Dependencies {
   lazy val trace4catsExporterCommon = "io.janstenpickle" %% "trace4cats-exporter-common" % Versions.trace4cats
   lazy val trace4catsKernel = "io.janstenpickle"         %% "trace4cats-kernel"          % Versions.trace4cats
   lazy val trace4catsModel = "io.janstenpickle"          %% "trace4cats-model"           % Versions.trace4cats
-  lazy val trace4catsExporterHttp = "io.janstenpickle"   %% "trace4cats-exporter-http"   % Versions.trace4catsExporterHttp
+  lazy val trace4catsExporterHttp = "io.janstenpickle" %% "trace4cats-exporter-http" % Versions.trace4catsExporterHttp
   lazy val trace4catsJaegerIntegrationTest =
     "io.janstenpickle" %% "trace4cats-jaeger-integration-test" % Versions.trace4catsJaegerIntegrationTest
 
-  lazy val circeGeneric = "io.circe"                       %% "circe-generic"                 % Versions.circe
-  lazy val collectionCompat = "org.scala-lang.modules"     %% "scala-collection-compat"       % Versions.collectionCompat
-  lazy val grpcApi = "io.grpc"                              % "grpc-api"                      % Versions.grpc
-  lazy val grpcOkHttp = "io.grpc"                           % "grpc-okhttp"                   % Versions.grpc
-  lazy val http4sBlazeClient = "org.http4s"                %% "http4s-blaze-client"           % Versions.http4s
-  lazy val http4sCirce = "org.http4s"                      %% "http4s-circe"                  % Versions.http4s
-  lazy val openTelemetrySdk = "io.opentelemetry"            % "opentelemetry-sdk"             % Versions.openTelemetry
-  lazy val openTelemetryOtlpExporter = "io.opentelemetry"   % "opentelemetry-exporter-otlp"   % Versions.openTelemetry
+  lazy val circeGeneric = "io.circe"                     %% "circe-generic"               % Versions.circe
+  lazy val collectionCompat = "org.scala-lang.modules"   %% "scala-collection-compat"     % Versions.collectionCompat
+  lazy val grpcApi = "io.grpc"                            % "grpc-api"                    % Versions.grpc
+  lazy val grpcOkHttp = "io.grpc"                         % "grpc-okhttp"                 % Versions.grpc
+  lazy val http4sBlazeClient = "org.http4s"              %% "http4s-blaze-client"         % Versions.http4s
+  lazy val http4sCirce = "org.http4s"                    %% "http4s-circe"                % Versions.http4s
+  lazy val openTelemetrySdk = "io.opentelemetry"          % "opentelemetry-sdk"           % Versions.openTelemetry
+  lazy val openTelemetryOtlpExporter = "io.opentelemetry" % "opentelemetry-exporter-otlp" % Versions.openTelemetry
   lazy val openTelemetryJaegerExporter = "io.opentelemetry" % "opentelemetry-exporter-jaeger" % Versions.openTelemetry
 
   lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
