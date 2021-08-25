e6a2c3393186d6dd0dd9d883048a28d394be9c91
Scala Steward <me@scala-steward.org>
2021-08-25 04:51:58 +0200

    Update grpc-api, grpc-okhttp to 1.40.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
     val circe = "0.14.1"
     val collectionCompat = "2.5.0"
-    val grpc = "1.40.0"
+    val grpc = "1.40.1"
     val http4s = "0.23.1"
     val openTelemetry = "1.5.0"
 
