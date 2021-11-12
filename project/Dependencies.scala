e8729a8b6d461e07989703cf22fc4a7a333197d6
Scala Steward <me@scala-steward.org>
2021-11-12 09:06:00 +0100

    Update opentelemetry-exporter-jaeger, ... to 1.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val collectionCompat = "2.5.0"
     val grpc = "1.42.1"
     val http4s = "0.23.6"
-    val openTelemetry = "1.7.1"
+    val openTelemetry = "1.9.0"
 
     val kindProjector = "0.13.2"
     val betterMonadicFor = "0.3.1"
