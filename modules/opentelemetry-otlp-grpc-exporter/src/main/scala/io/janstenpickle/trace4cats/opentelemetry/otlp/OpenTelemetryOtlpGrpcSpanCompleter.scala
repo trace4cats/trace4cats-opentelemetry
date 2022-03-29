e0b4b7c364dcc0f9d53c357c25eb2f60e92daf55
Brendan Maguire <1093243+brendanmaguire@users.noreply.github.com>
2022-03-29 10:08:41 +0100

    PR Change: Header keys are of type `CIString`

--- modules/opentelemetry-otlp-grpc-exporter/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/otlp/OpenTelemetryOtlpGrpcSpanCompleter.scala
+++ modules/opentelemetry-otlp-grpc-exporter/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/otlp/OpenTelemetryOtlpGrpcSpanCompleter.scala
@@ -5,6 +5,7 @@ import fs2.Chunk
 import io.janstenpickle.trace4cats.`export`.{CompleterConfig, QueuedSpanCompleter}
 import io.janstenpickle.trace4cats.kernel.SpanCompleter
 import io.janstenpickle.trace4cats.model.TraceProcess
+import org.typelevel.ci.CIString
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
@@ -15,7 +16,7 @@ object OpenTelemetryOtlpGrpcSpanCompleter {
     port: Int = 4317,
     config: CompleterConfig = CompleterConfig(),
     protocol: String = "http",
-    staticHeaders: Map[String, String] = Map.empty,
+    staticHeaders: Map[CIString, String] = Map.empty,
   ): Resource[F, SpanCompleter[F]] =
     Resource.eval(Slf4jLogger.create[F]).flatMap { implicit logger: Logger[F] =>
       OpenTelemetryOtlpGrpcSpanExporter[F, Chunk](host, port, protocol, staticHeaders)
