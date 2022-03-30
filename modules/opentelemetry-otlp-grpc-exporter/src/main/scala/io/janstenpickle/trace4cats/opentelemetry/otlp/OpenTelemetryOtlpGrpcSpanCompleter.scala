d9235beba6aa03fb2636686cb8472b721802ac1d
Brendan Maguire <1093243+brendanmaguire@users.noreply.github.com>
2022-03-30 21:13:50 +0100

    Allow static headers for OTLP exporter

--- modules/opentelemetry-otlp-grpc-exporter/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/otlp/OpenTelemetryOtlpGrpcSpanCompleter.scala
+++ modules/opentelemetry-otlp-grpc-exporter/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/otlp/OpenTelemetryOtlpGrpcSpanCompleter.scala
@@ -5,6 +5,7 @@ import fs2.Chunk
 import io.janstenpickle.trace4cats.`export`.{CompleterConfig, QueuedSpanCompleter}
 import io.janstenpickle.trace4cats.kernel.SpanCompleter
 import io.janstenpickle.trace4cats.model.TraceProcess
+import org.typelevel.ci.CIString
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
@@ -14,10 +15,11 @@ object OpenTelemetryOtlpGrpcSpanCompleter {
     host: String = "localhost",
     port: Int = 4317,
     config: CompleterConfig = CompleterConfig(),
-    protocol: String = "http"
+    protocol: String = "http",
+    staticHeaders: Map[CIString, String] = Map.empty,
   ): Resource[F, SpanCompleter[F]] =
     Resource.eval(Slf4jLogger.create[F]).flatMap { implicit logger: Logger[F] =>
-      OpenTelemetryOtlpGrpcSpanExporter[F, Chunk](host, port, protocol)
+      OpenTelemetryOtlpGrpcSpanExporter[F, Chunk](host, port, protocol, staticHeaders)
         .flatMap(QueuedSpanCompleter[F](process, _, config))
     }
 }
--- modules/opentelemetry-otlp-grpc-exporter/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/otlp/OpenTelemetryOtlpGrpcSpanExporter.scala
+++ modules/opentelemetry-otlp-grpc-exporter/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/otlp/OpenTelemetryOtlpGrpcSpanExporter.scala
@@ -5,15 +5,22 @@ import cats.effect.kernel.{Async, Resource}
 import io.janstenpickle.trace4cats.kernel.SpanExporter
 import io.janstenpickle.trace4cats.opentelemetry.common.{Endpoint, OpenTelemetryGrpcSpanExporter}
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
+import org.typelevel.ci.CIString
 
 object OpenTelemetryOtlpGrpcSpanExporter {
   def apply[F[_]: Async, G[_]: Foldable](
     host: String = "localhost",
     port: Int = 4317,
-    protocol: String = "http"
+    protocol: String = "http",
+    staticHeaders: Map[CIString, String] = Map.empty,
   ): Resource[F, SpanExporter[F, G]] =
     OpenTelemetryGrpcSpanExporter(
-      Endpoint(protocol, host, port),
-      ep => OtlpGrpcSpanExporter.builder().setEndpoint(ep.render).build()
+      endpoint = Endpoint(protocol, host, port),
+      makeExporter = endpoint =>
+        staticHeaders
+          .foldLeft(OtlpGrpcSpanExporter.builder().setEndpoint(endpoint.render)) { case (builder, (key, value)) =>
+            builder.addHeader(key.toString, value)
+          }
+          .build(),
     )
 }
