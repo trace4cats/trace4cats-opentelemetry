cbb8ede9ca8b988860b8fb5da11bd5b8ba976781
Scala Steward <scala_steward@virtuslab.com>
2022-07-21 18:45:31 +0000

    Update scala-collection-compat to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
     val autoValue = "1.9"
     val circe = "0.14.2"
-    val collectionCompat = "2.7.0"
+    val collectionCompat = "2.8.0"
     val grpc = "1.46.0"
     val http4s = "0.23.12"
     val openTelemetry = "1.14.0"
