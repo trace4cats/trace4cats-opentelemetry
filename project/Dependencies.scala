69b3bcd1578f5cc5985f6877d2703084fa8ab218
Scala Steward <scala_steward@virtuslab.com>
2022-08-02 18:16:47 +0000

    Update grpc-api, grpc-okhttp, grpc-stub to 1.48.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val autoValue = "1.9"
     val circe = "0.14.2"
     val collectionCompat = "2.8.1"
-    val grpc = "1.48.0"
+    val grpc = "1.48.1"
     val http4s = "0.23.14"
     val openTelemetry = "1.16.0"
 
